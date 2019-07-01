4c097b9b10c5d7c40ebe58005138dae66945024a
sarthfrey <sarth.frey@gmail.com>
2019-06-30 19:47:35 -0700

    update test case for direct model call

---
+++ ensemble/ensemble.py
@@ -0,0 +1,50 @@
+from collections import defaultdict
+
+
+# Both used for decoration and regular object instantiation
+class Ensemble(object):
+  model_functions = {}
+  ensemble_groups = defaultdict(set)
+  ensemble = ''
+
+  def __init__(self, specifier, *ensemble_names):
+    model_function = None
+    if callable(specifier):
+      model_function = specifier
+      ensemble_names = set(ensemble_names)
+    else:
+      ensemble_names = set([specifier])
+    if any(not ensemble_name for ensemble_name in ensemble_names):
+      raise ValueError('Must provide a valid ensemble names')
+    if model_function is None:
+      self.ensemble = specifier
+    else:
+      self.ensemble_groups[model_function.__name__] |= ensemble_names
+      self.model_functions[model_function.__name__] = model_function
+
+  def __call__(self, *args, **kwargs):
+    model = kwargs.get('model')
+    if callable(model):
+      raise ValueError('model should not be callable')
+      return self.__model
+    else:
+      return self.__model(*args, **kwargs)
+
+  def __model(self, *args, **kwargs):
+    model = kwargs.get('model')
+    if model not in self.model_functions or model not in self.ensemble_groups:
+      raise ValueError('There is no model function `{}` decorated with @Model'.format(model))
+    model_function = self.model_functions[model]
+    ensemble_group = self.ensemble_groups[model]
+    if self.ensemble not in ensemble_group:
+      raise ValueError('Model function `{}` is not attached to ensemble `{}`'.format(model, self.ensemble))
+    kwargs.pop('model', None)
+    return model_function(*args, **kwargs)
+
+
+class Model(object):
+  def __init__(self, *ensemble_names):
+    self.ensemble_names = ensemble_names
+
+  def __call__(self, model_function):
+    return Ensemble(model_function, *self.ensemble_names)
---
+++ example.py
@@ -0,0 +1,43 @@
+from ensemble import Model, Ensemble
+
+
+def h():
+  pass
+
+@Model('e1', 'e2')
+def f(x, y=3, z=4):
+  return x + y + z
+
+@Model('e1')
+def g(y):
+  return y**3
+
+
+if __name__ == '__main__':
+
+  # create our first ensemble and give it a name
+  e1 = Ensemble('e1')
+  # create a second ensemble
+  e2 = Ensemble('e2')
+
+  # you may use the ensembles as long as you specify which model you use
+  print(e1(model='f', x=2))
+  print(e1(model='g', y=3))
+  print(e2(model='f', x=2))
+
+  # try to use model `g` but it's not in ensemble `e2`
+  try:
+    print(e2(model='g', y=3))
+  except ValueError:
+    pass
+
+  # try to use model `h` but it's not decorated with @Model
+  try:
+    print(e1(model='h', y=3))
+  except ValueError:
+    pass
+
+  # you may specify your arguments positionally as usual
+  print(e1(3, model='f'))
+
+  print(f(1))
