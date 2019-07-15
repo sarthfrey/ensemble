5b6fdda4f044cc2b0e96041be27905797190f5c8
Sarth Frey <sarth.frey@gmail.com>
2019-07-14 19:57:38 -0700

    try this for docs

--- docs/conf.py
+++ docs/conf.py
@@ -30,8 +30,11 @@ release = '0.0.2'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 
+              'sphinx.ext.intersphinx', 'sphinx.ext.todo',
+              'sphinx.ext.ifconfig', 'sphinx.ext.viewcode',
+              'sphinx.ext.inheritance_diagram',
+              'sphinx.ext.autosummary']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
